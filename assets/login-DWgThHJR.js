5c3710013e160118e036faa6f2f4617d772850a3
salchavan <salvadorcastellanosposse@gmail.com>
2026-02-11 13:43:08 -0300

    Updates

--- assets/login-DWgThHJR.js
+++
@@ -1,1 +0,0 @@
-import"./tailwindcss-BhcMIiBe.js";const t=()=>{const e=document.getElementById("login-form");e&&e.addEventListener("submit",n=>{n.preventDefault(),window.location.href="/museo-web/index-admin.html"})};document.readyState==="loading"?document.addEventListener("DOMContentLoaded",t):t();
